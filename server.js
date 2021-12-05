f8fddc4c5bf90e4df09896190b4b1e72f2c3b1cf
Luis Guillermo Neisa Lopez <84110168+neisalg@users.noreply.github.com>
2021-12-05 10:10:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/Prueba1/<src>'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname+'/Prueba/<src>/index.html'));  
+    res.sendFile(path.join(__dirname+'/Prueba1/<src>/index.html'));  
 })
 
 app.listen(process.env.PORT || 8080);
